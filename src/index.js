29818293cd8a2d77d2aa076cc76a75c22bf12a2b
Anthony Flores <anthony.flores2293@gmail.com>
2020-03-18 14:32:44 -0700

    update

--- src/index.js
+++ src/index.js
@@ -11,11 +11,21 @@ const schema = gql`
     users: [User!]
     user(id: ID!): User
     me: User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 `;
 
@@ -23,10 +33,25 @@ let users = {
   1: {
     id: '1',
     username: 'Robin Wieruch',
+    messageIds: [1],
   },
   2: {
     id: '2',
     username: 'Dave Davids',
+    messageIds: [2],
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Bye World',
+    userId: '2',
   },
 };
 
@@ -43,11 +68,25 @@ const resolvers = {
     me: (parent, args, { me }) => {
       return me;
     },
+    messages: () => {
+      return Object.values(messages);
+    },
+    message: (parent, { id }) => {
+      return messages[id];
+    },
   },
 
   User: {
-    username: user => {
-      return user.username;
+    messages: user => {
+      return Object.values(messages).filter(
+        message => message.userId === user.id,
+      );
+    },
+  },
+
+  Message: {
+    user: message => {
+      return users[message.userId];
     },
   },
 };
