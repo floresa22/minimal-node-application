8851428f00dff5d38a751ae93b7eb1bcfd831409
Anthony Flores <anthony.flores2293@gmail.com>
2020-03-18 11:57:54 -0700

    'update'

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
 import 'dotenv/config';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const app = express();
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
+const schema = gql`
+  type Query {
+    me: User
+    user(id: ID!): User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin Wieruch',
+      };
+    },
+    user: (parent, args) => {
+      return {
+        username: 'Dave Davids',
+      };
+    },
+  },
 };
 
-console.log(user);
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
 
-console.log(process.env.SOME_ENV_VARIABLE);
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
+
+// const userCredentials = { firstname: 'Robin' };
+// const userDetails = { nationality: 'German' };
+
+// const user = {
+//   ...userCredentials,
+//   ...userDetails,
+// };
+
+// console.log(user);
+
+// console.log(process.env.SOME_ENV_VARIABLE);
+
+// const data = {
+//   me: {
+//     username: 'Robin Wieruch',
+//   },
+// };
